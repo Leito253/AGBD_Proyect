3baca5d84a0beea986ed1dbfd1111dbfd5e2be0d
Eric Aguirre <eric.aguirreet12d1@gmail.com>
2025-09-03 15:56:06 -0300

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,34 +1,34 @@
--- USUARIO
+-- USUARIOS
 INSERT INTO Usuario (Nombre, Contrasenia)
 VALUES 
 ('JuanPerez', 'ghYt57823V'),
 ('AnaGarcia', '178efgye34'),
 ('CarlosGomez', '4reuy45e8e'),
 ('LauraMendez', 'yt82jxnbv4'),  
-('PedroMartinez', 'xuy5kjw39r'),
-('RocioDiaz', 'PaSsW098'),
-('SofiaLopez', 'n9y43mndk'),
+('PedroMartinez', 'xuy5kjw39r');
 
 -- MANGAS
 INSERT INTO Mangas (Nombre, Lanzamiento, Genero, Precio, Autor, Editorial)
-VALUES ('Naruto', '1999-09-21', 'Shonen', 15990, 'Masashi Kishimoto', 'Shueisha'),
+VALUES 
+('Naruto', '1999-09-21', 'Shonen', 15990, 'Masashi Kishimoto', 'Shueisha'),
 ('One Piece', '1997-07-22', 'Shonen', 18500, 'Eiichiro Oda', 'Shueisha'),
 ('Dragon Ball Z', '1989-04-15', 'Shonen', 17990, 'Akira Toriyama', 'Shueisha'),
 ('Kimetsu no Yaiba', '2016-02-15', 'Shonen', 20000, 'Koyoharu Gotouge','Shueisha');
 
 -- EMPLEADOS
-INSERT INTO Empleados (Nombre, Apellido, dni, Cuil, SueldoUSD)
+INSERT INTO Empleados (Cuil, Nombre, Apellido, dni, SueldoUSD)
 VALUES 
-('Carlos', 'Martinez', 49783457, 20497834571, 1500),
-('Maria', 'Lopez', 46983456, 20469834562, 1600),
-('Luis', 'Ramirez', 46284732, 20465893745, 1450);
+(20497834571,'Carlos', 'Martinez', 49783457, 1500),
+(20469834562,'Maria', 'Lopez', 46983456, 1600),
+(20475893745,'Luis', 'Ramirez', 46284732, 1450);
 
 -- COMPRAS
-INSERT INTO Compra (idUsuario, idManga, PrecioTotal, NroCompra)
-VALUES (1, 1, 15990, 101),
-(2, 2, 18500, 102),
-(3, 3, 17990, 103),
-(4, 4, 21000, 104);
+INSERT INTO Compra (NroCompra, idUsuario, idManga, PrecioTotal)
+VALUES 
+(101, 1, 1, 15990),
+(102, 2, 2, 18500),
+(103, 3, 3, 17990),
+(104, 4, 4, 21000);
 
 -- SUCURSALES
 INSERT INTO Sucursales (Ciudad, Direccion)
@@ -38,26 +38,20 @@ VALUES
 ('Buenos Aires', 'Juncal 1770');
 
 -- DEVOLUCIONES 
-INSERT INTO Devoluciones (idUsuario, Nrocompra, Motivo)
+INSERT INTO Devoluciones (NroCompra, idUsuario, Motivo)
 VALUES  
-(1, 101, 'Manga equivocado, pedí otro título'),      
-(2, 102, 'El manga llegó incompleto'),
-(3, 103, 'No era el volumen que necesitaba');
+(101, 1, 'Manga equivocado, pedí otro título'),      
+(102, 2, 'El manga llegó incompleto'),
+(103, 3, 'No era el volumen que necesitaba');
 
 -- CALIFICACIÓN
-INSERT INTO Calificacion (idUsuario, idManga, Puntaje, Comentarios)
-VALUES  (1, 1, 5, 'Excelente manga, atrapante desde el inicio'),
-(2, 2, 4, 'Muy bueno, aunque a veces se hace largo'),
-(3, 3, 5, 'Obra maestra, lo recomiendo totalmente'),
-(4, 4, 3, 'Está bien, pero esperaba más acción'),
-(5, 1, 4, 'Me gusta mucho, aunque no es mi favorito');
-
--- DEVOLUCIONES 
-INSERT INTO Devoluciones (idUsuario, Nrocompra, Motivo)
+INSERT INTO Calificacion (idUsuario, idManga, Puntaje, Comentarios, FechaHora)
 VALUES  
-(1, 101, 'Manga equivocado, pedí otro título'),      
-(2, 102, 'El manga llegó incompleto'),
-(3, 103, 'No era el volumen que necesitaba');
+(1, 1, 5, 'Excelente manga, atrapante desde el inicio', ),
+(2, 2, 4, 'Muy bueno, aunque a veces se hace largo', ),
+(3, 3, 5, 'Obra maestra, lo recomiendo totalmente', ),
+(4, 4, 3, 'Está bien, pero esperaba más acción', ),
+(5, 1, 4, 'Me gusta mucho, aunque no es mi favorito', );
 
 -- STOCK
 INSERT INTO Stock (idManga, idSucursal, Cantidad)
@@ -68,11 +62,8 @@ VALUES
 (4, 1, 250);
 
 -- ENVÍOS 
-INSERT INTO Envios (Ciudad, Calle, Direccion, CodigoPostal, Referencia)
+INSERT INTO Envios (NroCompra, Ciudad, Calle, Direccion, CodigoPostal, Referencia)
 VALUES 
-('La Plata', 'Hipolito Yrigoyen', '1132', 1407, 'En medio de un kiosco'),
-('Corrientes','Callao' , '243', 5000, 'Frente a la plaza'),
-('Rosario', 'San Martin', '1451', 2000, 'Cerca del centro comercial');
-
-
-
+(101, 'La Plata', 'Hipolito Yrigoyen', 1132, 1407, 'En medio de un kiosco'),
+(102, 'Corrientes', 'Callao', 243, 5000, 'Frente a la plaza'),
+(103, 'Rosario', 'San Martin', 1451, 2000, 'Cerca del centro comercial');
